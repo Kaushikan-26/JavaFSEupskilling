137b865ed97294ce92e7c92094fc068543a67251
Kaushikan-26 <kaushikan008@gmail.com>
2025-07-03 23:36:14 -0700

    Delete module-3 directory

--- module-3/Sample.java
+++
@@ -1,9 +0,0 @@
-public class Sample {
-    public String greet(String name) {
-        return "Hello, " + name;
-    }
-
-    public static void main(String[] args) {
-        System.out.println(new Sample().greet("World"));
-    }
-}
